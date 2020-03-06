1cb9018f32aa722b6aaad37b5198faebb01cc9c6
ericy <ericy@stud.ntnu.no>
2020-03-06 17:44:53 +0100

    restructure main control flow

---
+++ MotorControl.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+
+
+class MotorControl(object):
+    
+    def __init__(self):
+        self.speed = 100
+
+        #setup motors, and communication with motorHAT
+        # create a default object, no changes to I2C address or frequency
+        self.mh = Adafruit_MotorHAT(addr=0x60)
+        
+        ################################# Set motors
+        self.motorA = self.mh.getMotor(1)
+        self.motorB = self.mh.getMotor(2)
+        
+        # set the speed to start, from 0 (off) to 255 (max speed)
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+        self.motorA.run(Adafruit_MotorHAT.FORWARD)
+        self.motorB.run(Adafruit_MotorHAT.FORWARD)
+        # turn on motor
+        self.motorA.run(Adafruit_MotorHAT.RELEASE)
+        self.motorB.run(Adafruit_MotorHAT.RELEASE)
+        
+        
+    def turnOffMotors(self):
+        print("Turning off motors")
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+    def driveForward(self):
+        print("Driving forward")
+        self.motorA.run(Adafruit_MotorHAT.FORWARD)
+        self.motorB.run(Adafruit_MotorHAT.FORWARD)
+
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+
+    def driveRevers(self):
+        print("Reversing")
+        self.motorA.run(Adafruit_MotorHAT.BACKWARD)
+        self.motorB.run(Adafruit_MotorHAT.BACKWARD)
+        
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+
+    def pivotLeft(self):
+        print("pivoting left")
+        self.motorA.run(Adafruit_MotorHAT.FORWARD)
+        self.motorB.run(Adafruit_MotorHAT.BACKWARD)
+        
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+
+    def pivotRight(self):
+        print("pivoting right")
+        self.motorA.run(Adafruit_MotorHAT.BACKWARD)
+        self.motorB.run(Adafruit_MotorHAT.FORWARD)
+        
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+    
+    def stop(self):
+        print("Stopping")
+        self.speed = 0
+        self.motorA.setSpeed(self.speed)
+        self.motorB.setSpeed(self.speed)
+
+    def speedUp(self):
+        print("Speeding up")
+        self.speed += 5
+    
+    def slowDown(self):
+        print("Slowing down")
+        self.speed -= 5
+    
+
+
+
+# auto-disables motors on shutdown!
+atexit.register(vaccuBot.turnOffMotors)
--- distanceSense.py
+++ distanceSense.py
@@ -2,34 +2,34 @@ import RPi.GPIO as gpio
 import time
 
 
-def distance():
-    TRIG = 12
-    ECHO = 16
-    try:
-        gpio.setmode(gpio.BOARD) # OR BCM depending on layout chosen
-        gpio.setup(TRIG, gpio.OUT)
-        gpio.setup(ECHO, gpio.IN)
-        gpio.output(TRIG, False)
+class Sensors:
 
-        while gpio.input(ECHO) == 0:
-            start = time.time()
+    def distance(self):
+        TRIG = 12
+        ECHO = 16
+        try:
+            gpio.setmode(gpio.BOARD) # OR BCM depending on layout chosen
+            gpio.setup(TRIG, gpio.OUT)
+            gpio.setup(ECHO, gpio.IN)
+            gpio.output(TRIG, False)
 
-        while gpio.input(ECHO) == 1:
-            stop = time.time()
+            while gpio.input(ECHO) == 0:
+                start = time.time()
 
-        duration = stop - start
+            while gpio.input(ECHO) == 1:
+                stop = time.time()
 
-      
-        distance = duration / 0.000058
+            duration = stop - start
 
-        gpio.cleanup()
-        return distance
+        
+            distance = duration / 0.000058
 
-    except:
-        distance = 100
-        gpio.cleanup()
-        return distance
+            gpio.cleanup()
+            return distance
+
+        except:
+            distance = 100
+            gpio.cleanup()
+            return distance
 
 
-if __name__ == "__main__":
-    print(distance())
